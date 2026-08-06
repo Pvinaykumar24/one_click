1c98ccfd55b49dfbf5c2cb5c857da83ee8eb52b5
Pvinaykumar24 <cs24i1028@iiitdm.ac.in>
2026-08-07 01:17:22 +0530

    Fix debugPrint import and clean unused methods for 0 analyze errors

--- one_click_app/lib/services/mess_service.dart
+++ one_click_app/lib/services/mess_service.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:flutter/foundation.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'admin_service.dart';
